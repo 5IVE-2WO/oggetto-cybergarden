374c713e318a74dbca420ad7eb16606018d3bb75
N3WERS <space.galaxy@list.ru>
2023-12-24 00:17:33 +0300

    added redux login slice and authentication level

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
-import {route} from "./route";
+import {adminsRoute, userRoute, guestRoute} from "./route";
+import {useSelector} from "react-redux";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            {route.map(route =>
-                <Route path={route.path} element={route.component} exact={route.path}/>
-            )}
-        </Routes>
-    );
+    const authState = useSelector(state => state.auth)
+    console.log(authState.authLevel)
+    switch (authState.authLevel) {
+        case 1:
+            return (
+                <Routes>
+                    {guestRoute.map(route => <Route path={route.path} element={route.component} exact={route.path}/>)}
+                </Routes>
+            )
+
+        case 2:
+            return (
+                <Routes>
+                    {userRoute.map(route => <Route path={route.path} element={route.component} exact={route.path}/>)}
+                </Routes>
+            )
+
+        case 3:
+            return (
+                <Routes>
+                    {adminsRoute.map(route => <Route path={route.path} element={route.component} exact={route.path}/>)}
+                </Routes>
+            )
+        default:
+            return (
+                <Routes>
+                    <Route path="*" element={guestRoute[1].component}/>
+                </Routes>
+            )
+    }
 };
 
 export default AppRouter;
