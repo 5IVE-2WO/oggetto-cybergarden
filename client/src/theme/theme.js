d9a35c3cc07dfb643fa4c4073a183c25b7376fa6
oriboni <guardnight62@gmail.com>
2023-12-23 12:26:47 +0300

    Finish theme

--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -1,15 +1,21 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#FFF343',
-            light: '#FFF5B5',
-            dark: '#FFB042',
-            black: '#1A1A1A',
-            white: '#F4FFEE',
-        },
+  palette: {
+    primary: {
+      main: "#FFF343",
+      light: "#FFF5B5",
+      dark: "#FFB042",
+      black: "#1A1A1A",
+      white: "#F4FFEE",
     },
+  },
+  typography: {
+    fontFamily: "Montserat, Regular",
+    addition: {
+      fontFamily: "Georgia, Regular",
+    },
+  },
 });
 
 export default theme;
