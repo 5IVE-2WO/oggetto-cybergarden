9d672d5bd6089f9d9fd36c4219f12ec5797b0e09
maksim <maksim.maestto@gmail.com>
2023-12-24 12:43:36 +0300

    DB template

---
+++ server/db_template.sql
@@ -0,0 +1,5 @@
+INSERT INTO speakers (name, specialization) values ('Черненко Максим Игоревич', 'Сомнолог');
+INSERT INTO speakers (name, specialization) values ('Пушкин Александр Сергеевич', 'Поэт');
+
+INSERT INTO events (title, content, timestamp, speaker_id) values ('Разбираем проблемы со сном', 'Контент снов', '2023-01-01 12:30:00', 1)
+INSERT INTO events (title, content, timestamp, speaker_id) values ('Урок литературы)', 'Пушкин', '2023-01-02 15:30:00', 2)
